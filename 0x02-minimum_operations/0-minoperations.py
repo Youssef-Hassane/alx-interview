b1184689747af4ead64b24416c5186498b383737
youssef-hassane <youssef_hassane@icloud.com>
2024-08-09 01:41:04 +0300

    DONE

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-""" Minimum Operations
-    """
+"""
+Minimum Operations
+"""
 
 
 def minOperations(n: int) -> int:
-    """ Minimum Operations needed to get n H characters """
-    next = 'H'
+    """
+    Minimum Operations needed to get n H characters
+    """
+    next_char = 'H'
     body = 'H'
     op = 0
+
     while (len(body) < n):
         if n % len(body) == 0:
             op += 2
-            next = body
+            next_char = body
             body += body
         else:
             op += 1
-            body += next
+            body += next_char
     if len(body) != n:
         return 0
     return op
